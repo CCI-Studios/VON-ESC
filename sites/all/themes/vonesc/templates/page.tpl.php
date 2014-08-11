6f8fd57422fc56e1d800dca74d9bdd317a7f1824
Chris Morris <cmorris@ccistudios.com>
2014-08-11 11:43:14 -0400

    Remove action links from node pages
    close #20

--- sites/all/themes/vonesc/templates/page.tpl.php
+++ sites/all/themes/vonesc/templates/page.tpl.php
@@ -105,7 +105,6 @@
         <?php print $messages; ?>
         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
       </div></div></div> <!-- /#content -->
 
