2c0ce551c1f3853eff364f896c1e869bb3a96733
Chris Morris <cmorris@ccistudios.com>
2014-10-16 09:37:43 -0400

    Update server address in drush alias

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -3,7 +3,7 @@
 $aliases['dev'] = array(
 	'uri'=> 'dev.voneriestclair.ca',
 	'root' => '/home/vonesc/subdomains/dev/public_html',
-	'remote-host'=> 'host.lambtonshield.com',
+	'remote-host'=> 'host.cciserver2.com',
 	'remote-user'=> 'vonesc',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
