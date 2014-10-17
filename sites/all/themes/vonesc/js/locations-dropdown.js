826512ddc332f70bc0151b6c7919b5497ce13d9b
Chris Morris <cmorris@ccistudios.com>
2014-10-17 11:19:42 -0400

    Support locations drop downs with fewer than 5 items

--- sites/all/themes/vonesc/js/locations-dropdown.js
+++ sites/all/themes/vonesc/js/locations-dropdown.js
@@ -2,25 +2,39 @@
     $(function(){
         $("ul.locations-dropdown").each(function(){
             $(this).find("li:first-child").after("<a href='#' class='open-btn'>Open Dropdown</a>");
-            $(this).find("a.open-btn").click(open);
+            $(this).find("a.open-btn").click(toggle);
             $(this).find("a").click(function(e){
                 e.stopPropagation();
             });
             $(this).after("<div class='locations-dropdown-placeholder'>&nbsp;</div>");
         });
 
-        $("body").click(close);
+        $("body").click(closeAll);
     });
-
-    function open(e)
+    
+    function toggle()
     {
-        $(this).parents("ul.locations-dropdown").toggleClass("open");
-
+        $ul = $(this).parents("ul.locations-dropdown");
+        if ($ul.hasClass("open"))
+        {
+            closeAll();
+        }
+        else
+        {
+            open($ul);
+        }
         return false;
     }
 
-    function close()
+    function open($ul)
     {
-        $("ul.locations-dropdown").removeClass("open");
+        $ul.addClass("open").animate({"max-height":"204px"});
+    }
+
+    function closeAll()
+    {
+        $("ul.locations-dropdown.open").each(function(){
+            $(this).removeClass("open").css("max-height",$(this).outerHeight()).animate({"max-height":"40px"});
+        });
     }
 }(jQuery));
