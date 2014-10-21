2229536c7c5eff20c015722629fe5bfc48cc9d7d
Chris Morris <cmorris@ccistudios.com>
2014-10-21 10:26:14 -0400

    make location page bg not cover under the modules
    close #96

--- sites/all/themes/vonesc/js/location-page.js
+++ sites/all/themes/vonesc/js/location-page.js
@@ -7,7 +7,7 @@
     function fixLocationBackground()
     {
         $("body.node-type-location #block-system-main .field-name-field-image img").each(function(){
-            var height = $("#main-wrapper").height() + $("#page-title").height() - 10;
+            var height = $(".field-name-body").height() + $(".tabs").height() + $("#page-title").height() - 10;
             $(this).height(height).css("margin-top", -1*($("#page-title").height() - 5) + "px");
         });
     }
