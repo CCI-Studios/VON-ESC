ed99fa9cbcb5c32f6a73bbdcdbf11fdf34a0d52f
Chris Morris <cmorris@ccistudios.com>
2014-11-10 09:47:04 -0500

    Added drush alias for live

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,12 @@ $aliases['dev'] = array(
 	)
 );
 
+$aliases['live'] = array(
+	'uri'=> 'live.voneriestclair.ca',
+	'root' => '/home/vonesc/subdomains/live/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'vonesc',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
