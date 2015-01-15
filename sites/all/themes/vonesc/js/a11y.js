a46443feb7411b9f933e00bb0ca2b33c0cc73eae
Matt Elliott <melliott@ccistudios.com>
2015-01-15 11:31:05 -0500

    a11y menu script inserts text into anchor

--- sites/all/themes/vonesc/js/a11y.js
+++ sites/all/themes/vonesc/js/a11y.js
@@ -2,7 +2,7 @@
     $(function(){
         $("#block-system-main-menu .content > ul > li").each(function(){
             var text = $(this).find("> a").text();
-            $(this).find("ul li").prepend("<span class='element-invisible'>"+text+"</span>");
+            $(this).find("ul li a").prepend("<span class='element-invisible'>"+text+" </span>");
         });
     });
 }(jQuery));
